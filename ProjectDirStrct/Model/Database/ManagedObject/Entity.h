7dd740be7c283138b446eb18d95ca1704ea4c3e8
员员 <syxiaqj@gmail.com>
2014-04-30 11:34:25 +0800

    创建项目
    创建项目

---
+++ ProjectDirStrct/Model/Database/ManagedObject/Entity.h
@@ -0,0 +1,17 @@
+//
+//  Entity.h
+//  ProjectDirStrct
+//
+//  Created by xqj on 14-4-30.
+//  Copyright (c) 2014年 renhe. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+
+
+@interface Entity : NSManagedObject
+
+@property (nonatomic, retain) NSString * eid;
+
+@end
---
+++ ProjectDirStrct/Model/Database/ManagedObject/Entity.m
@@ -0,0 +1,16 @@
+//
+//  Entity.m
+//  ProjectDirStrct
+//
+//  Created by xqj on 14-4-30.
+//  Copyright (c) 2014年 renhe. All rights reserved.
+//
+
+#import "Entity.h"
+
+
+@implementation Entity
+
+@dynamic eid;
+
+@end
