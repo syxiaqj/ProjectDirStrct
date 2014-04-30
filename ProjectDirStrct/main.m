eebcf00bc33d4c33d07f542a6788945e9dab8938
员员 <syxiaqj@gmail.com>
2014-04-30 09:55:45 +0800

    Initial Commit

---
+++ ProjectDirStrct/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ProjectDirStrct
+//
+//  Created by xqj on 14-4-30.
+//  Copyright (c) 2014年 renhe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
